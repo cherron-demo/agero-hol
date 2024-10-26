33d0460d6f97b4b7d98aaef8602d57f48c0d1059
cherron-demo <carterherron@gmail.com>
2024-10-25 20:33:57 -0400

    Update teardown.sql

--- admin/teardown.sql
+++ admin/teardown.sql
@@ -7,6 +7,11 @@ call utility.public.loopquery('drop database if exists HOL;', $num_users);
 call utility.public.loopquery('drop user if exists userXXX;', $num_users);
 call utility.public.loopquery('drop role if exists roleXXX;', $num_users);
 call utility.public.loopquery('drop warehouse if exists whXXX;', $num_users);
+
+drop database WORLDWIDE_ADDRESS_DATA;
+drop database CARTO_ACADEMY__DATA_FOR_TUTORIALS;
 drop database utility;
 
 show users;
+show warehouses;
+show roles;
