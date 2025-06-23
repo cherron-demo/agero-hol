96a857f06a7786b6d6436535bf58b538b8026a5d
cherron-demo <carterherron@gmail.com>
2025-06-23 12:58:31 -0700

    Update utility.sql

--- admin/utility.sql
+++ admin/utility.sql
@@ -1,70 +1,27 @@
-use schema hol.public;
---line 20 need number of participants & line 21 needs password set
-create or replace database utility;
+------Overall-------
 
--- create a SP to loop queries for N users
--- it replaces the placeholder XXX with N in the supplied query
-create or replace procedure utility.public.loopquery (QRY STRING, N FLOAT)
-  returns float
-  language javascript
-  strict
-as
-$$
-  for (i = 0; i <= N; i++) {
-    snowflake.execute({sqlText: QRY.replace(/XXX/g, i)});
-  }
-  return i-1;
-$$;
+--Make sure everyone has a Snowflake login
+--give each attendee an ID (e.g. 01, 02, 03, etc)
 
--- Set up the HOL environment for the first time
-set num_users = 2; --> adjust number of attendees here
-set lab_pwd = ''; --> enter an attendee password here
+create or replace role AGERO_HOL_ROLE; 
+grant role AGERO_HOL_ROLE to role accountadmin;
 
--- set up the roles
-create or replace role hol_parent comment = "HOL parent role";
-use role accountadmin;
-grant role hol_parent to role accountadmin;
-call utility.public.loopquery('create or replace role roleXXX comment = "HOLXXX User Role";', $num_users);
+--this needs to be per attendee so each person has an individual warehouse
+CREATE OR REPLACE WAREHOUSE <WH_XX> WITH WAREHOUSE_SIZE='XSMALL' AND AUTO_SUSPEND = 300;
+grant ownership on warehouse <WH_XX> to role AGERO_HOL_ROLE;
 
--- Create Cortex role
-CREATE or replace ROLE cortex_user_role;
-GRANT DATABASE ROLE SNOWFLAKE.CORTEX_USER TO ROLE cortex_user_role;
 
--- set up the users
-call utility.public.loopquery('create or replace user userXXX default_role=roleXXX password="' || $lab_pwd || '";', $num_users);
-call utility.public.loopquery('grant role roleXXX to user userXXX;', $num_users);
-call utility.public.loopquery('grant role roleXXX to role hol_parent;', $num_users);
-call utility.public.loopquery('grant role roleXXX to role accountadmin;', $num_users);
+grant usage on database AGERO_HOL_DB to role AGERO_HOL_ROLE;
+grant ownership on schema AGERO_HOL_DB.CALL_TRANSCRIPTS to role AGERO_HOL_ROLE;
+grant ownership on schema AGERO_HOL_DB.IMAGE_CLASSIFICATION to role AGERO_HOL_ROLE;
+grant ownership on schema AGERO_HOL_DB.ML_FUNCTIONS to role AGERO_HOL_ROLE;
 
--- grant account permissions
-grant create warehouse on account to role hol_parent;
 
------setup warehouse didn't run bc it doesn't exist
---grant usage on warehouse setup_wh to role hol_parent;
+------Forecasting------
 
--- set up the warehouses and grant permissions
-call utility.public.loopquery('create or replace warehouse whXXX warehouse_size = \'xsmall\' AUTO_SUSPEND = 300;', $num_users);
-call utility.public.loopquery('grant all on warehouse whXXX to role roleXXX;', $num_users);
+GRANT CREATE SNOWFLAKE.ML.FORECAST ON SCHEMA AGERO_HOL_DB.ML_FUNCTIONS TO ROLE AGERO_HOL_ROLE;
 
--- set up the schemas and grant permissions
-call utility.public.loopquery('create or replace schema HOL.schemaXXX clone HOL.PUBLIC;', $num_users);
-call utility.public.loopquery('grant usage, modify on database HOL to role roleXXX;', $num_users);
-call utility.public.loopquery('grant usage on schema HOL.PUBLIC to role roleXXX;', $num_users);
-call utility.public.loopquery('grant ownership on schema HOL.PUBLIC to role roleXXX revoke current grants;', $num_users);
-call utility.public.loopquery('grant ownership on schema HOL.schemaXXX to role roleXXX;', $num_users);
-call utility.public.loopquery('grant usage, modify on future schemas in database HOL to role roleXXX;', $num_users);
-call utility.public.loopquery('grant all on all tables in schema HOL.schemaXXX to role roleXXX;', $num_users);
-call utility.public.loopquery('grant all on all views in schema hol.schemaXXX to role roleXXX;', $num_users);
-call utility.public.loopquery('grant all on future views in schema hol.schemaXXX to role roleXXX;', $num_users);
-call utility.public.loopquery('GRANT usage ON schema hol.schemaXXX TO ROLE roleXXX', $num_users);
-call utility.public.loopquery('GRANT create stage ON schema hol.schemaXXX TO ROLE roleXXX', $num_users);
-call utility.public.loopquery('GRANT usage ON warehouse whXXX TO ROLE roleXXX', $num_users);
-call utility.public.loopquery('GRANT operate ON warehouse whXXX TO ROLE roleXXX', $num_users);
-call utility.public.loopquery('GRANT monitor ON warehouse whXXX TO ROLE roleXXX', $num_users);
-call utility.public.loopquery('GRANT modify ON warehouse whXXX TO ROLE roleXXX', $num_users);
-call utility.public.loopquery('GRANT ROLE cortex_user_role TO ROLE roleXXX', $num_users);
-call utility.public.loopquery('GRANT CREATE STREAMLIT ON SCHEMA hol.schemaXXX TO ROLE roleXXX', $num_users);
-call utility.public.loopquery('GRANT CREATE STAGE ON SCHEMA hol.schemaXXX TO ROLE roleXXX', $num_users);
-call utility.public.loopquery('GRANT CREATE NOTEBOOK ON SCHEMA hol.schemaXXX TO ROLE roleXXX', $num_users);
+------Image Classification------
 
-SHOW USERS;
+GRANT CREATE NOTEBOOK ON SCHEMA AGERO_HOL_DB.IMAGE_CLASSIFICATION TO ROLE AGERO_HOL_ROLE;
+
